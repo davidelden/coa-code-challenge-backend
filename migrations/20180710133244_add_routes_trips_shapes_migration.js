15aa13b3591acdfa2fc7e070bf9a59111c60e551
wedesignstudios <deelden@gmail.com>
2018-07-11 13:18:22 -0500

    add shape_sequences table, change datatype of service_id to string, add cascade delete to FKs, add indexing

--- migrations/20180710133244_add_routes_trips_shapes_migration.js
+++ migrations/20180710133244_add_routes_trips_shapes_migration.js
@@ -22,6 +22,15 @@ exports.up = function(knex, Promise) {
         return knex.schema.createTable('shapes', (tbl) => {
           tbl.increments('id').primary();
           tbl.integer('shape_id').unique();
+          tbl.timestamps(true, true);
+        })
+      }
+    }),
+    knex.schema.hasTable('shape_sequences').then((exists) => {
+      if(!exists) {
+        return knex.schema.createTable('shape_sequences', (tbl) => {
+          tbl.increments('id').primary();
+          tbl.integer('shape_id').references('shapes.shape_id').onDelete('CASCADE').index();
           tbl.float('shape_pt_lat');
           tbl.float('shape_pt_lon');
           tbl.integer('shape_pt_sequence');
@@ -34,14 +43,14 @@ exports.up = function(knex, Promise) {
       if(!exists) {
         return knex.schema.createTable('trips', (tbl) => {
           tbl.increments('id').primary();
-          tbl.integer('route_id').references('routes.route_id');
-          tbl.integer('service_id');
+          tbl.integer('route_id').references('routes.route_id').onDelete('CASCADE').index();
+          tbl.string('service_id');
           tbl.integer('trip_id');
           tbl.string('trip_headsign');
           tbl.string('trip_short_name');
           tbl.boolean('direction_id');
           tbl.integer('block_id');
-          tbl.integer('shape_id').references('shapes.shape_id');
+          tbl.integer('shape_id').references('shapes.shape_id').onDelete('CASCADE').index();
           tbl.specificType('wheelchair_accessible', 'smallint');
           tbl.specificType('bikes_allowed', 'smallint');
           tbl.string('dir_abbr');
@@ -54,7 +63,8 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTable('routes')
+    .dropTable('trips')
+    .dropTable('shape_sequences')
     .dropTable('shapes')
-    .dropTable('trips')    
+    .dropTable('routes')
 };
