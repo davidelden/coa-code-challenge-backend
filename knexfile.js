4a4d2e1064b8e40233e6ff0c3abdbea24655b7c3
wedesignstudios <deelden@gmail.com>
2018-07-15 12:31:00 -0500

    update knexfile with staging, production config

--- knexfile.js
+++ knexfile.js
@@ -19,42 +19,28 @@ module.exports = {
 
   staging: {
     client: 'pg',
-    connection: {
-      host:     process.env.POSTGRESQL_HOST_STAGING,
-      database: process.env.POSTGRESQL_DB_NAME_STAGING,
-      user:     process.env.POSTGRESQL_USER_STAGING,
-      password: process.env.POSTGRESQL_PASSWORD_STAGING
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    debug: true,
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     },
     seeds: {
       directory: './seeds'
-    }
+    },
+    ssl: true
   },
 
   production: {
     client: 'pg',
-    connection: {
-      host:     process.env.POSTGRESQL_HOST,
-      database: process.env.POSTGRESQL_DB_NAME,
-      user:     process.env.POSTGRESQL_USER,
-      password: process.env.POSTGRESQL_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    debug: true,
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     },
     seeds: {
       directory: './seeds'
-    }
+    },
+    ssl: true
   }
 
 };
