9bbe89c2bbd360013e5ec80ce94608f6aa84a2ac
wedesignstudios <deelden@gmail.com>
2018-07-15 12:15:41 -0500

    replace db string with env variable, export app module

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const { postgraphile } = require('postgraphile');
 
-app.use(postgraphile('postgres://localhost:5432/coa_gtfs_cap_metro_data', 'public', {graphiql: true}));
+app.use(postgraphile(process.env.DATABASE_URL, 'public', {graphiql: true}));
 
-app.listen(3000, () => console.log('Now browse to localhost:3000/graphql'));
+module.exports = app;
